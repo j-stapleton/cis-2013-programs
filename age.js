7db410490ee85caace9cd2cac2481be3eb0a8748
stapleton <89820852+j-stapleton@users.noreply.github.com>
2021-09-17 19:28:44 -0500

    Update age.js

--- age.js
+++ age.js
@@ -2,6 +2,6 @@ var floatAge, floatDays, floatMonths, intWeeks, intFortnights;
 floatAge = parseFloat(prompt("Enter age in years"));
 floatDays = floatAge*365.25;
 floatMonths = floatAge*12;
-intWeeks = floatDays/7
-intFortnights = floatDays/14
-alert("Age = " + floatDays, floatMonths, intWeeks, intFortnights);
+intWeeks = floatDays/7;
+intFortnights = floatDays/14;
+alert("Age = " + floatDays + floatMonths + intWeeks + intFortnights);
