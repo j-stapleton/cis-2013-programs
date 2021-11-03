49502e774ef51636d91a01a239a6859432eeb7e3
stapleton <89820852+j-stapleton@users.noreply.github.com>
2021-11-03 13:36:56 -0500

    Update HO7.js

--- HO7.js
+++ HO7.js
@@ -8,38 +8,14 @@ var $ = function (id)
 //this function performs all the circle, triangle, and circle calculations and prints the results
 var do_it = function () 
 {
+alert ("The Rectangle area is " + rectArea(floatRectWidth,floatRectHeight) + 
+	" and the perimeter is " + rectPerim (floatRectWidth,floatRectHeight) + 
+	"\n\n" + "The Triangle area is " + 	triArea(floatTriSide1,floatTriSide2,floatTriSide3) + 
+	" and the perimeter is " + 
+	triPerim(floatTriSide1,floatTriSide2,floatTriSide3) + "\n\n" +
+	"The Circle area is " + circleArea(floatRadius) + 
+	" and the circumference is " + circleCircum(floatRadius));
 
-//lines 12-18 declares and sets variables with respective DOM input. lines 19-25 declare variables needed for calculations
-	var floatRectWidth = parseFloat($("rect_width").value);
-	var floatRectHeight = parseFloat($("rect_height").value);	
-	var floatTriSide1 = parseFloat($("tri_side1").value);
-	var floatTriSide2 = parseFloat($("tri_side2").value);	
-	var floatTriSide3 = parseFloat($("tri_side3").value);
-	var floatRadius = parseFloat($("radius").value);
-	var floatRectArea = parseFloat($("rect_area").value);
-	var floatRectPerim = parseFloat($("rect_perim").value);
-	var floatS = parseFloat($("S").value);
-	var floatTriangleArea = parseFloat($("triangle_area").value);
-	var floatTrianglePerim = parseFloat($("triangle_perim").value);
-	var floatCircleArea = parseFloat($("circle_area").value);
-	var floatCircleCircum = parseFloat($("circle_circum").value);	
-
-//calculate the rectangle area and perimeter
-	floatRectArea = floatRectWidth * floatRectHeight;
-	floatRectPerim = 2 * floatRectWidth + 2 * floatRectHeight;
-
-//calculate the triangle area using Heron's formula (don't remember it? look it up...) and perimeter	
-	floatS = (floatTriSide2 + floatTriSide2 + floatTriSide3)/2;
-	floatTriangleArea = Math.sqrt((floatS*(floatS-floatTriSide1)*(floatS-floatTriSide2)*(floatS-floatTriSide3)));
-	floatTrianglePerim = floatTriSide1 + floatTriSide2 + floatTriSide3;
-
-//calculate the circle area and circumference using convenient Math Object methods	
-	floatCircleArea = Math.PI * Math.pow(floatRadius,2);
-	floatCircleCircum = 2 * Math.PI * floatRadius;  
-	
-	alert ("The Rectangle area is " + floatRectArea.toFixed(2) + " and the perimeter is " + floatRectPerim.toFixed(2) + "\n\n" +
-		"The Triangle area is " + floatTriangleArea.toFixed(2) + " and the perimeter is " + floatTrianglePerim.toFixed(2) + "\n\n" +
-		"The Circle area is " + floatCircleArea.toFixed(2) + " and the circumference is " + floatCircleCircum.toFixed(2));
 
 };
 
