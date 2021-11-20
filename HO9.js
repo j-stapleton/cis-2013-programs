c0e59b414e093cda492f7b86f931350629d18658
stapleton <89820852+j-stapleton@users.noreply.github.com>
2021-11-19 23:21:58 -0600

    Update HO9.js

--- HO9.js
+++ HO9.js
@@ -10,9 +10,9 @@ var $ = function (id)
 var generate = function () 
 {
 	var intCount = parseInt($("total_fib").value);  //gets user input from the DOM
-	while (isNaN(intCount) || intCount > 0 && intCount<100)
+	while (isNaN(intCount) || intCount < 0 && intCount > 100) //using a while function to check if the inputted # is good 
 	{
-		intCount = parseInt(prompt("Your number was invalid, try another."));
+		intCount = parseInt(prompt("Your number was invalid, try another.")); //Using a prompt to alert the user if the number is invalid
 	}	
 	//validate user input that intCount is a number and intCount is not less than 2 or intCount not greater than 100
 	
@@ -23,7 +23,7 @@ var generate = function ()
 
 	// write whatever kind of loop you want, given the above info, to generate the appropriate
 	// number of fibonacci numbers and then post it to the html DOM 
-for (let i=1; i<=intCount; i++)
+for (let i=1; i<=intCount; i++) // For loop to start the fibonacci set
 {
     k = i + j;
     i = j;
